b7455f58a3fbef0ed1e0fa2a890f09fb3480b730
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-08-09 20:38:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song = "";
 
+leftwristX = 0;
+leftwristY = 0;
+
+rightwristX = 0;
+rightwristY = 0;
+
 function preload()
 {
     song = loadSound("music.mp3");
@@ -8,6 +14,8 @@ function preload()
 function play()
 {
     song.play()
+    song.volume(1);
+    song.rate(1);
 }
 
 function setup()
@@ -17,6 +25,29 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Posenet is online");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftwristX + "leftWristY = " + leftwristY);
+
+        rightwristX = results[0].pose.rightWrist.x;
+        rightwristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightwristX + "rightWristY = " + rightwristY);
+    }
 }
 
 function draw()
