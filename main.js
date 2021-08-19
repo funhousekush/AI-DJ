b8006f70ad2cee0c9e7d133691aab55b077fc1a8
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-08-19 15:38:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ leftwristY = 0;
 rightwristX = 0;
 rightwristY = 0;
 
+scoreleftwrist = 00;
+scorerightwrist = 00;
+
 function preload()
 {
     song = loadSound("music.mp3");
@@ -14,7 +17,7 @@ function preload()
 function play()
 {
     song.play()
-    song.volume(1);
+    song.setVolume(1);
     song.rate(1);
 }
 
@@ -39,6 +42,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreleftwrist = results[0].pose.keypoints[9].score;
+        scorerightwrist = results[0].pose.keypoints[10].score;
+        console.log("score left wrist = " + scoreleftwrist + "score right wrist = " + scorerightwrist);
 
         leftwristX = results[0].pose.leftWrist.x;
         leftwristY = results[0].pose.leftWrist.y;
@@ -53,6 +59,49 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreleftwrist > 0.2)
+    {    
+        circle(leftwristX, leftwristY, 20);
+        var number_format = Number(leftwristY);
+        number_it = floor(number_format);
+        volume =  number_it/500;
+        document.getElementById("Volume").innerHTML = "=" + volume;
+        song.setVolume(volume);
+    }
+    if(scorerightwrist > 0.2)
+    {
+        circle(rightwristX, rightwristY, 20);
+
+        if(rightwristY > 0 && rightwristY <= 100)
+        {
+            document.getElementById("Speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
+        if(rightwristY > 100 && rightwristY <= 200)
+        {
+            document.getElementById("Speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+        if(rightwristY > 200 && rightwristY <= 300)
+        {
+            document.getElementById("Speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+        if(rightwristY > 300 && rightwristY <= 400)
+        {
+            document.getElementById("Speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+        if(rightwristY > 400 && rightwristY <= 500)
+        {
+            document.getElementById("Speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5);
+        }
+    }
 }
 
 function Stop()
